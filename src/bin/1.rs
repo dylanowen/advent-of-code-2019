4a4be0e5c97d842e287bb54c5bab3763e10ea3e7
Dylan Owen <dylan.owen@workday.com>
2019-12-02 19:57:32 -0800

    updated 1

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,31 +1,46 @@
+use advent_of_code_2019::{run, Problem};
+
+struct One {}
+
+impl Problem for One {
+    type Input = Vec<isize>;
+
+    fn parse(s: &str) -> Vec<isize> {
+        s.split('\n')
+            .map(|mass| mass.parse::<isize>().expect("parse error"))
+            .collect()
+    }
+
+    fn part_1(modules_mass: &Vec<isize>, _is_example: bool) -> Option<String> {
+        let fuel_requirements: isize = modules_mass.iter().map(|mass| mass / 3 - 2).sum();
+
+        Some(format!("{}", fuel_requirements))
+    }
+
+    fn part_2(modules_mass: &Vec<isize>, _is_example: bool) -> Option<String> {
+        let fuel_requirements: isize = modules_mass
+            .iter()
+            .map(|mass| {
+                let mut result = 0;
+                let mut last_fuel = mass / 3 - 2;
+                while last_fuel > 0 {
+                    result += last_fuel;
+                    last_fuel = last_fuel / 3 - 2;
+                }
+
+                result
+            })
+            .sum();
+
+        Some(format!("{}", fuel_requirements))
+    }
+
+    fn problem_number() -> usize {
+        1
+    }
+}
+
 fn main() {
-    let input = include_str!("1_input.txt");
-
-    let modules_mass: Vec<isize> = input
-        .split('\n')
-        .map(|mass| {
-            mass.parse::<isize>()
-                .expect("we should be able to parse this")
-        })
-        .collect();
-
-    let fuel_requirements: isize = modules_mass.iter().map(|mass| mass / 3 - 2).sum();
-
-    println!("Part 1: {}", fuel_requirements);
-
-    let fuel_requirements: isize = modules_mass
-        .iter()
-        .map(|mass| {
-            let mut result = 0;
-            let mut last_fuel = mass / 3 - 2;
-            while last_fuel > 0 {
-                result += last_fuel;
-                last_fuel = last_fuel / 3 - 2;
-            }
-
-            result
-        })
-        .sum();
-
-    println!("Part 2: {}", fuel_requirements);
+    run::<One>(true, "100756");
+    run::<One>(false, include_str!("1_input.txt"));
 }
